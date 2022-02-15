1894efccbdbe5b5dbfeee57c16bfa35d89019864
Maryanne Wachter <maryanne.wachter@gmail.com>
2022-02-14 16:51:30 -0800

    changed colorscale and fixed domain name

--- shell_scripts/source.sh
+++ shell_scripts/source.sh
@@ -1,5 +1,5 @@
 source ~/.zshrc  
 workon tinky-care
 python3 ${HOME}/workspaces/current/tinky-care/bots/orgbot.py
-scp ${HOME}/workspaces/current/tinky-care/assets/org.png pi@raspberrypi.local:~/projects/tinky-care/assets
+scp ${HOME}/workspaces/current/tinky-care/assets/org.png pi@raspberrypi.attlocal.net:~/projects/tinky-care/assets
 deactivate
